06e5f71144c24fa92de68d14ac7cff2ad7329d0d
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-04-09 19:12:44 +0300

    Added NLogger and Annotations to project. Configured Swagger options

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.OpenApi.Models;
+using NLog;
+using NLog.Web;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
 namespace API
 {
     public class Program
@@ -8,7 +14,23 @@ namespace API
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            });
+
+            var swagger = GetSwaggerGenOptions();
+            builder.Services.AddSwaggerGen(swagger);
+
+            LogManager
+                .Setup()
+                .LoadConfigurationFromAppSettings()
+                .GetCurrentClassLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Host.UseNLog();
 
             var app = builder.Build();
 
@@ -22,6 +44,33 @@ namespace API
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
+        }
+
+        private static Action<SwaggerGenOptions> GetSwaggerGenOptions()
+        {
+            return options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GameDatabase API",
+                    Description = "GameDatabase internal API specification",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "mail",
+                        Email = "tsyganok.vasya@yandex.ru"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "License",
+                        Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                    }
+                });
+
+                string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename) ?? null);
+                options.EnableAnnotations();
+            };
         }
     }
 }
