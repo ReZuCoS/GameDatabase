bc835531d90a7d0b2178239134f62aad969975a4
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-06-18 20:45:00 +0300

    Added HttpContextClaims extractor

---
+++ API/Utils/HttpContextClaims.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace API.Utils
+{
+    public class HttpContextClaims
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public HttpContextClaims(IHttpContextAccessor httpContext)
+        {
+            _context = httpContext;
+        }
+
+        public string? Get(string id)
+        {
+            return _context.HttpContext?.User.FindFirstValue(id);
+        }
+    }
+}
