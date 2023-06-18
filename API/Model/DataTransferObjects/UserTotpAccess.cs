75a97554a703c997eb4337b8634bf36f435bf34c
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-06-18 20:46:32 +0300

    Added UserTotpAccess storage class

---
+++ API/Model/DataTransferObjects/UserTotpAccess.cs
@@ -0,0 +1,27 @@
+﻿namespace API.Controllers
+{
+    /// <summary>
+    /// User totp access storage
+    /// </summary>
+    public class UserTotpAccess
+    {
+        /// <summary>
+        /// User totp access storage constructor
+        /// </summary>
+        public UserTotpAccess(string accessToken, string totpKey)
+        {
+            AccessToken = accessToken;
+            TotpKey = totpKey;
+        }
+
+        /// <summary>
+        /// User's totp key
+        /// </summary>
+        public string TotpKey { get; set; }
+
+        /// <summary>
+        /// User's access token
+        /// </summary>
+        public string AccessToken { get; set; }
+    }
+}
